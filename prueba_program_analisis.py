14e2500bf0dc522148235b5d59a798ee71bc31a6
bourloti <bourlotignacio97@gmail.com>
2025-01-30 18:24:21 -0300

    ...

--- prueba_program_analisis.py
+++ prueba_program_analisis.py
@@ -164,6 +164,8 @@ class GeneraArchivoCSV:
 def generar_df_totalizadores_balanzas(df_dia, titulo):
     '''
     Genera un dataframe con los valores por hora de todas las balanzas, y lo convierte a CSV
+    df_dia: lista donde cada elementeo es un dataframe con columna1 (fecha) y columna 2 (totalizador)
+    titulo: lista con los nombres de cada una de las balanzas
     '''
     
     # Inicializa el diccionario vacío
@@ -193,25 +195,26 @@ def main():
     """
     # Diccionario con las rutas de los archivos CSV
     archivos = {
-        'balanza_final_mb': 'D:/Mantenimiento/Bourlot Ignacio/GitHub repositorios/caudal_balanzas/proyecto_caudal_balanza/caudal_balanza_final_molino_blanco.csv',
-        'balanza_ingreso_mb': 'D:/Mantenimiento/Bourlot Ignacio/GitHub repositorios/caudal_balanzas/proyecto_caudal_balanza/caudal_balanza_ingreso_molino_blanco.csv',
-        'balanza_integral_mb': 'D:/Mantenimiento/Bourlot Ignacio/GitHub repositorios/caudal_balanzas/proyecto_caudal_balanza/caudal_balanza_integral_molino_blanco.csv',
-        'balanza_final_mp': 'D:/Mantenimiento/Bourlot Ignacio/GitHub repositorios/caudal_balanzas/proyecto_caudal_balanza/caudal_balanza_final_molino_parboil.csv',
-        'balanza_ingreso_mp': 'D:/Mantenimiento/Bourlot Ignacio/GitHub repositorios/caudal_balanzas/proyecto_caudal_balanza/caudal_balanza_ingreso_molino_parboil.csv',
-        'balanza_integral_mp': 'D:/Mantenimiento/Bourlot Ignacio/GitHub repositorios/caudal_balanzas/proyecto_caudal_balanza/caudal_balanza_integral_molino_parboil.csv',
-        'balanza_silo_101': 'D:/Mantenimiento/Bourlot Ignacio/GitHub repositorios/caudal_balanzas/proyecto_caudal_balanza/caudal_balanza_silo_101.csv',
-        'balanza_tempering': 'D:/Mantenimiento/Bourlot Ignacio/GitHub repositorios/caudal_balanzas/proyecto_caudal_balanza/caudal_balanza_tempering.csv',
-        'balanza_materia_prima': 'D:/Mantenimiento/Bourlot Ignacio/GitHub repositorios/caudal_balanzas/proyecto_caudal_balanza/caudal_balanza_ingreso_materia_prima.csv'
+        'balanza_final_mb': 'C:\progrmasPython\proyecto_caudal_balanza\proyecto_caudal_balanza\caudal_balanza_final_molino_blanco.csv',
+        'balanza_ingreso_mb': 'C:\progrmasPython\proyecto_caudal_balanza\proyecto_caudal_balanza\caudal_balanza_ingreso_molino_blanco.csv',
+        'balanza_integral_mb': 'C:\progrmasPython\proyecto_caudal_balanza\proyecto_caudal_balanza\caudal_balanza_integral_molino_blanco.csv',
+        'balanza_final_mp': 'C:\progrmasPython\proyecto_caudal_balanza\proyecto_caudal_balanza\caudal_balanza_final_molino_parboil.csv',
+        'balanza_ingreso_mp': 'C:\progrmasPython\proyecto_caudal_balanza\proyecto_caudal_balanza\caudal_balanza_ingreso_molino_parboil.csv',
+        'balanza_integral_mp': 'C:\progrmasPython\proyecto_caudal_balanza\proyecto_caudal_balanza\caudal_balanza_integral_molino_parboil.csv',
+        'balanza_silo_101': 'C:\progrmasPython\proyecto_caudal_balanza\proyecto_caudal_balanza\caudal_balanza_silo_101.csv',
+        'balanza_tempering': 'C:\progrmasPython\proyecto_caudal_balanza\proyecto_caudal_balanza\caudal_balanza_tempering.csv',
+        'balanza_materia_prima': 'C:\progrmasPython\proyecto_caudal_balanza\proyecto_caudal_balanza\caudal_balanza_ingreso_materia_prima.csv'
     }
 
     # Crear una instancia para cada archivo CSV usando AcondicionadorCSV, el keys es el nombre de la balanza y el value es el objeto creado
     objetos_balanzas = {nombre: AcondicionadorCSV(ruta) for nombre, ruta in archivos.items()}
     
     titulos_graficos = [titulo for titulo in archivos.keys()]
-    df_dia_graficos = []
+    
 
     while True:
         try:
+            df_dia_graficos = []
             # Ejecutar el proceso a las 00:05:00
             if input('Enter') == '1':
             #if str(datetime.now().strftime("%H:%M:%S")) == '03:23:00':
@@ -223,27 +226,36 @@ def main():
                     df_acondicionado = balanza.acondicionar_archivo_CSV()
 
                     if df_acondicionado is None:
-                        # Crear objeto de calculador para calcular las toneladas por día
-                        calculador = CalculadorToneladasDia(df_acondicionado)
                         df_dia_graficos.append(0)
                         continue
 
-                    # Crear objeto de calculador para calcular las toneladas por día, y lo guarda en una lista, para luego poder graficarlos
+                    # Crear objeto de calculador para calcular las toneladas por día, crea un DataFrame con fecha y caudal por hora. Guarda el df en una lista, para luego poder graficarlos
                     calculador = CalculadorToneladasDia(df_acondicionado)
                     df_dia_graficos.append(calculador.calcular_toneladas_dia(fecha_formateada))
 
-                    if df_dia_graficos is None:
-                        continue
-            
+                    # if df_dia_graficos is None:
+                    #     continue
+
+                #-------------------------------------------------------------------------------------------------------
                 # Si los datos fueron calculados correctamente, graficar los resultados
-                for i in range(len(df_dia_graficos)):
-                    graficador = Graficador(df_dia_graficos[i], titulos_graficos[i].upper())
-                    graficador.graficar(fecha_formateada)
-                
+                # for i in range(len(df_dia_graficos)):
+                #     graficador = Graficador(df_dia_graficos[i], titulos_graficos[i].upper())
+                #     graficador.graficar(fecha_formateada)
+                #-------------------------------------------------------------------------------------------------------
+
+                #-------------------------------------------------------------------------------------------------------
+                # Crea un archivo CSV con los datos de fecha y totalizador balanza por hora
                 df_totalizadores_balanzas = generar_df_totalizadores_balanzas(df_dia_graficos, titulos_graficos)
-                totalizadores_por_hora = GeneraArchivoCSV('totalizadores_todas_balanzas.csv', 'D:\Mantenimiento\Bourlot Ignacio\GitHub repositorios\caudal_balanzas\proyecto_caudal_balanza')
+                totalizadores_por_hora = GeneraArchivoCSV('totalizadores_todas_balanzas.csv', 'C:\progrmasPython\proyecto_caudal_balanza\proyecto_caudal_balanza')
                 totalizadores_por_hora.crear_csv(df_totalizadores_balanzas) 
-                      
+
+                df = pd.read_csv("totalizadores_todas_balanzas.csv")
+                # Eliminar filas duplicadas (mantiene la primera ocurrencia por defecto)
+                df_sin_duplicados = df.drop_duplicates()
+                # Guardar el nuevo CSV sin duplicados
+                df_sin_duplicados.to_csv("totalizadores_todas_balanzas.csv", index=False)
+                #-------------------------------------------------------------------------------------------------------
+
         except KeyboardInterrupt:
             print("Proceso interrumpido por el usuario.")
             break
